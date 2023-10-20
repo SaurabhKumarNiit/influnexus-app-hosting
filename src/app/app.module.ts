dea6b6b63e43bc4f81d2cae854cc37b80076fc70
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2023-10-20 18:39:24 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -91,7 +93,9 @@ import { NavbarComponent } from './navbar/navbar.component';
           console.error(err);
         }
       } as SocialAuthServiceConfig,
-    }
+    },
+
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
